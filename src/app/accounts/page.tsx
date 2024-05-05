c502716d12eb2ed21f4eec717da86c85c0bbdd56
Daniela Barbosa <contato@danielacb.com>
2024-05-04 23:13:30 -0300

    add basic navigation

---
+++ src/app/accounts/page.tsx
@@ -0,0 +1,3 @@
+export default function Accounts() {
+  return <h2 className="text-3xl font-bold tracking-tight">Accounts</h2>
+}
---
+++ src/app/cards/page.tsx
@@ -0,0 +1,3 @@
+export default function Cards() {
+  return <h2 className="text-3xl font-bold tracking-tight">Cards</h2>
+}
